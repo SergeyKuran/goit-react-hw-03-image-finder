e0bb99c34521626b79b1aac02a239047567d741f
Sergey Kuran <kuran.serg@gmail.com>
2023-06-19 01:45:50 +0300

    Create Searchbar
    Create Searchbar component and styles css

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Component } from 'react';
+
+import css from './Searchbar.module.css';
+
+class Searchbar extends Component {
+  state = {};
+
+  render() {
+    return (
+      <header className={css.Searchbar}>
+        <form className={css.SearchForm}>
+          <button type="submit" className={css.SearchFormButton}>
+            <span className={css.SearchFormButtonLabel}>Search</span>
+          </button>
+
+          <input
+            className={css.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export default Searchbar;
