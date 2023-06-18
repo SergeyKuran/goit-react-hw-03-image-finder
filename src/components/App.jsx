3178ca9669cf9ca139edbf8eae97e1aaf3f60fb5
Sergey Kuran <kuran.serg@gmail.com>
2023-06-19 01:46:25 +0300

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import Searchbar from './Searchbar';
+
+import css from './App.module.css';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className={css.App}>
+        <Searchbar></Searchbar>
+      </div>
+    );
+  }
+}
+
+export default App;
