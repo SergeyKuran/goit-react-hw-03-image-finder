65111b6d1d3cb64f6e3ce4f8102eef430f746e6e
Sergey Kuran <kuran.serg@gmail.com>
2023-06-22 00:14:56 +0300

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-import { Component } from 'react';
-
+import React, { Component } from 'react';
 import Searchbar from './Searchbar';
 
 import css from './App.module.css';
+import ImageGallery from './ImageGallery';
+// import Modal from './Modal';
 
 class App extends Component {
-  state = {};
+  state = {
+    searchText: '',
+  };
+
+  onSubmit = text => {
+    this.setState({ searchText: text });
+  };
 
   render() {
+    const { searchText } = this.state;
     return (
       <div className={css.App}>
-        <Searchbar></Searchbar>
+        <Searchbar onSubmit={this.onSubmit} />
+        <ImageGallery text={searchText} />
       </div>
     );
   }
