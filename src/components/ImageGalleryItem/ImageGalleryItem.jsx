2621a574e05b793696ac155662b2704996ffc579
Sergey Kuran <kuran.serg@gmail.com>
2023-06-22 00:15:24 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -3,29 +3,31 @@ import css from './ImageGalleryItem.module.css';
 
 class ImageGalleryItem extends Component {
   state = {
-    searchText: this.props.searchText,
+    searchText: null,
   };
 
-  componentDidUpdate(prevState, nextState) {
+  componentDidUpdate(nextProps, nextState) {
     const BASE_URl = 'https://pixabay.com/api/';
     const API_KEY = '16104754-fccb05fa4a4190bcc2750c19f';
     const page = 1;
-    console.log('prevState :>> ', prevState);
-    console.log('nextState :>> ', nextState);
-    if (prevState.searchText !== this.state.searchText)
+
+    if (nextProps.searchText !== this.props.searchText) {
       fetch(
         `${BASE_URl}?key=${API_KEY}&q=${this.props.searchText}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
       )
         .then(resp => resp.json())
-        .then(data => console.log(data, 'data'));
+        .then(data => console.log(data));
+    }
   }
 
   render() {
-    console.log(this.props.searchText, 'sss');
-
     return (
       <li className={css.ImageGalleryItem}>
-        <img className={css.ImageGalleryItemImage} src="" alt="" />
+        <img
+          className={css.ImageGalleryItemImage}
+          src={this.props.searchText}
+          alt=""
+        />
       </li>
     );
   }
