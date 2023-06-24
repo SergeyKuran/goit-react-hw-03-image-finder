75eb21a79a8ae326ccab7e89d73df4234fc0c192
Sergey Kuran <kuran.serg@gmail.com>
2023-06-25 00:37:37 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -4,48 +4,21 @@ import PropTypes from 'prop-types';
 import css from './ImageGalleryItem.module.css';
 
 class ImageGalleryItem extends Component {
-  state = {
-    searchText: '',
-    error: null,
-    images: [],
-  };
-
-  componentDidUpdate(nextProps, _) {
-    const BASE_URl = 'https://pixabay.com/api/';
-    const API_KEY = '16104754-fccb05fa4a4190bcc2750c19f';
-    const page = 1;
-
-    if (nextProps.searchText !== this.props.searchText) {
-      fetch(
-        `${BASE_URl}?key=${API_KEY}&q=${this.props.searchText}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
-      )
-        .then(resp => {
-          if (resp.ok) {
-            return resp.json();
-          }
-          return Promise.reject(new Error());
-        })
-        .then(data =>
-          this.setState(prevState => ({
-            images: [...prevState.images, ...data.hits],
-          }))
-        )
-        .catch(error => this.setState({ error }));
-    }
-  }
-
-  getImages = () => {};
+  onClickModal = () => {};
 
   render() {
-    const { images } = this.state;
+    const { images } = this.props;
     return (
       <>
-        {images.map(({ id, largeImageURL }) => (
-          <li key={id} className={css.ImageGalleryItem}>
+        {images.map(({ id, largeImageURL, webformatURL }) => (
+          <li key={id} className={css.imageGalleryItem}>
             <img
-              className={css.ImageGalleryItemImage}
+              className={css.imageGalleryItemImage}
               src={largeImageURL}
               alt=""
+              onClick={() => {
+                this.onClickModal();
+              }}
             />
           </li>
         ))}
@@ -55,8 +28,8 @@ class ImageGalleryItem extends Component {
 }
 
 ImageGalleryItem.propTypes = {
-  searchText: PropTypes.string.isRequired,
-  error: PropTypes.bool.isRequired,
+  searchText: PropTypes.string,
+  error: PropTypes.node,
   images: PropTypes.arrayOf(PropTypes.object.isRequired),
 };
 
