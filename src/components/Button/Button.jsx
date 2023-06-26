b2501d2cc01b094bf01dbfbb99451f5abbf024b8
Sergey Kuran <kuran.serg@gmail.com>
2023-06-27 02:35:30 +0300

    Update Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,17 @@
 import css from './Button.module.css';
 
+import PropTypes from 'prop-types';
+
 const Button = ({ onClick }) => {
-  console.log(onClick);
   return (
     <button className={css.button} type="button" onClick={onClick}>
       Load more
     </button>
   );
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
